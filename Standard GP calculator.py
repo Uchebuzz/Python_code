01cc9cf1d689d1aae7a8beee1fa1e8b5c5b13ccb
Uche Buzz <buzugbeuche@gmail.com>
2023-02-16 09:22:21 +0100

    few changes

---
+++ Standard GP calculator.py
@@ -0,0 +1,34 @@
+# Ask the user to input the number of courses
+num_courses = int(input("Enter the number of courses: "))
+
+# Initialize variables
+total_points = 0
+total_credits = 0
+
+# Loop through each course
+for i in range(num_courses):
+    # Ask the user to input the grade and credit for the course
+    grade = (input("Enter the grade for course {}: ".format(i + 1)))
+    credits = int(input("Enter the credit for course {}: ".format(i + 1)))
+
+    # Calculate the grade points and add them to the total
+    if grade == "A":
+        grade_points = 5.0
+    elif grade == "B":
+        grade_points = 4.0
+    elif grade == "C":
+        grade_points = 3.0
+    elif grade == "D":
+        grade_points = 2.0
+    elif grade == "F":
+        grade_points = 0
+    else:
+        grade_points = 0.0
+    total_points += grade_points * credits
+
+    # Add the credits to the total
+    total_credits += credits
+
+# Calculate the GPA and print it
+gpa = total_points / total_credits
+print("Your GPA is {:.2f}".format(gpa))
