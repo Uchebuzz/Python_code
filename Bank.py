182608391cf9c68abc0fa5b1e7fab897464ef419
Buzugbe Uche <105880802+Uchebuzz@users.noreply.github.com>
2024-07-18 12:45:24 +0100

    Commit

--- Bank.py
+++ Bank.py
@@ -5,23 +5,35 @@
 import time
 
 def Deposit():
-    amount = float(input("Enter an amount to be deposited:$  "))
+    amount = float(input("Enter an amount to be deposited:$ "))
 
     if amount < 0:
         print("enter a valid amount") 
         return 0
     else:
-        print(f"Thank you for your deposit of {amount}, it is safe with us")
+        print(f"Thank you for your deposit of ${amount}, it is safe with us")
 
     return amount
     
     #bank_opening()
 def Withraw():
-    pass
+    to_be_withdrawn = float(input("How much do you want to withdraw? $ "))
+
+    if to_be_withdrawn < 0:
+        print("enter a valid amount") 
+        return 0
+    if to_be_withdrawn > balance:
+        print("insufficient amount")
+        return 0
+    else:
+        print(f"{to_be_withdrawn} has been deducted from your balance")
+        return to_be_withdrawn
+
 
 def show_balance():
     print(f"your balance is ${balance:.2f}")
-        
+    return balance
+
 balance = 0
 is_running =True
 
@@ -29,15 +41,15 @@ while is_running:
     print("Welcome to Bank of life")
     time.sleep(3)
     print("")
-    print("1. withdraw")
+    print("1. Withdraw")
     print("2. Deposit")
     print("3. Balance")
     print("4. Exit")
 
-    choice = input(("what would you like to do today? (1 - 4)    " ))
+    choice = input(("what would you like to do today? (1 - 4): "))
 
     if choice == "1":
-        Withraw()
+       balance -= Withraw()
     elif choice == "2":
        balance += Deposit()
     elif choice == "3":
